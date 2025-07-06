bd7827a6e2e8cd202c1cbc47e09c28c05f4dcebf
nickscha <nick@scharnbeck.de>
2025-07-06 19:14:04 +0200

    [TEST] generate more x86_64 executables for tests

--- tests/efb_test.c
+++ tests/efb_test.c
@@ -14,22 +14,89 @@ LICENSE
 
 #include "test.h" /* Simple Testing framework */
 
+void efb_test_hello_world(void)
+{
+  /* x64 Windows syscall-based hello world (hardcoded IAT & handles) */
+  static unsigned char machine_code_hello[] = {
+      /* sub rsp, 0x28 (shadow space) */
+      0x48, 0x83, 0xEC, 0x28,
+
+      /* mov ecx, -11 (STD_OUTPUT_HANDLE) */
+      0xB9, 0xF5, 0xFF, 0xFF, 0xFF,
+
+      /* call GetStdHandle (at RIP + offset) */
+      0xE8, 0x1B, 0x00, 0x00, 0x00, /* offset to GetStdHandle */
+
+      /* mov rcx, rax ; handle */
+      0x48, 0x89, 0xC1,
+
+      /* lea rdx, [rip + str_offset] ; message */
+      0x48, 0x8D, 0x15, 0x1B, 0x00, 0x00, 0x00,
+
+      /* mov r8d, 13 ; string length */
+      0x41, 0xB8, 0x0D, 0x00, 0x00, 0x00,
+
+      /* lea r9, [rsp + 0x20] ; lpNumberOfCharsWritten */
+      0x4C, 0x8D, 0x4C, 0x24, 0x20,
+
+      /* xor rax, rax ; lpReserved = NULL */
+      0x48, 0x31, 0xC0,
+
+      /* call WriteConsoleA */
+      0xE8, 0x1A, 0x00, 0x00, 0x00,
+
+      /* mov ecx, 0 ; ExitProcess(0) */
+      0xB9, 0x00, 0x00, 0x00, 0x00,
+
+      /* call ExitProcess */
+      0xE8, 0x17, 0x00, 0x00, 0x00,
+
+      /* add rsp, 0x28 ; restore stack */
+      0x48, 0x83, 0xC4, 0x28,
+
+      /* ret */
+      0xC3,
+
+      /* --- string "Hello, world!\n" --- */
+      'H', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd', '!', '\n',
+
+      /* --- IAT (Import Address Table) --- */
+      /* (dummy placeholder function pointers to patch manually or resolve dynamically if desired) */
+  };
+
+  assert(efb_build_executable("hello_world.exe", machine_code_hello, sizeof(machine_code_hello)));
+}
+
 int main(void)
 {
   /*
   ret
   */
-  unsigned char ret_undefined[] = {0xC3};
+  unsigned char machine_code_ret_undefined[] = {0xC3};
 
   /*
   mov eax, 0;
   ret
   */
-  unsigned char ret_valid[] = {0xB8, 0x00, 0x00, 0x00, 0x00, 0xC3};
+  unsigned char machine_code_ret_valid[] = {0xB8, 0x00, 0x00, 0x00, 0x00, 0xC3};
+
+  /*
+  ud2
+  */
+  unsigned char machine_code_illegal_opcode[] = {0x0F, 0x0B};
+
+  /*
+  jmp $
+  */
+  unsigned char machine_code_infinite_loop[] = {0xEB, 0xFE};
 
   /* Build executable format */
-  assert(efb_build_executable("ret_undefined.exe", ret_undefined, sizeof(ret_undefined)));
-  assert(efb_build_executable("ret_valid.exe", ret_valid, sizeof(ret_valid)));
+  assert(efb_build_executable("ret_undefined.exe", machine_code_ret_undefined, sizeof(machine_code_ret_undefined)));
+  assert(efb_build_executable("ret_valid.exe", machine_code_ret_valid, sizeof(machine_code_ret_valid)));
+  assert(efb_build_executable("illegal_opcode.exe", machine_code_illegal_opcode, sizeof(machine_code_illegal_opcode)));
+  assert(efb_build_executable("infinite_loop.exe", machine_code_infinite_loop, sizeof(machine_code_infinite_loop)));
+
+  efb_test_hello_world();
 
   return 0;
 }

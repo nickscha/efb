cfec015f1d93c63b64f81952bd6ac8615c5a5b77
nickscha <nick@scharnbeck.de>
2025-07-06 18:56:45 +0200

    [INITIAL] Ready Set Go

---
+++ README.md
@@ -0,0 +1,56 @@
+# efb
+A C89 standard compliant, single header, nostdlib (no C Standard Library) executable file/format builder (EFB).
+
+For more information please look at the "efb.h" file or take a look at the "examples" or "tests" folder.
+
+> [!WARNING]
+> THIS PROJECT IS A WORK IN PROGRESS! ANYTHING CAN CHANGE AT ANY MOMENT WITHOUT ANY NOTICE! USE THIS PROJECT AT YOUR OWN RISK!
+
+## Quick Start
+
+Download or clone efb.h and include it in your project.
+
+```C
+#include "efb.h"
+
+int main() {
+
+    /*
+      mov eax, 0;
+      ret
+    */
+    unsigned char x86_64_ret[] = {0xB8, 0x00, 0x00, 0x00, 0x00, 0xC3};
+
+    efb_build_executable("ret.exe", x86_64_ret, sizeof(x86_64_ret));
+
+    return 0;
+}
+```
+
+## Run Example: nostdlib, freestsanding
+
+In this repo you will find the "examples/efb_win32_nostdlib.c" with the corresponding "build.bat" file which
+creates an executable only linked to "kernel32" and is not using the C standard library and executes the program afterwards.
+
+## "nostdlib" Motivation & Purpose
+
+nostdlib is a lightweight, minimalistic approach to C development that removes dependencies on the standard library. The motivation behind this project is to provide developers with greater control over their code by eliminating unnecessary overhead, reducing binary size, and enabling deployment in resource-constrained environments.
+
+Many modern development environments rely heavily on the standard library, which, while convenient, introduces unnecessary bloat, security risks, and unpredictable dependencies. nostdlib aims to give developers fine-grained control over memory management, execution flow, and system calls by working directly with the underlying platform.
+
+### Benefits
+
+#### Minimal overhead
+By removing the standard library, nostdlib significantly reduces runtime overhead, allowing for faster execution and smaller binary sizes.
+
+#### Increased security
+Standard libraries often include unnecessary functions that increase the attack surface of an application. nostdlib mitigates security risks by removing unused and potentially vulnerable components.
+
+#### Reduced binary size
+Without linking to the standard library, binaries are smaller, making them ideal for embedded systems, bootloaders, and operating systems where storage is limited.
+
+#### Enhanced performance
+Direct control over system calls and memory management leads to performance gains by eliminating abstraction layers imposed by standard libraries.
+
+#### Better portability
+By relying only on fundamental system interfaces, nostdlib allows for easier porting across different platforms without worrying about standard library availability.
---
+++ tests/efb_test.c
@@ -0,0 +1,77 @@
+/* efb.h - v0.1 - public domain data structures - nickscha 2025
+
+A C89 standard compliant, single header, nostdlib (no C Standard Library) executable file/format builder (EFB).
+
+This Test class defines cases to verify that we don't break the excepted behaviours in the future upon changes.
+
+LICENSE
+
+  Placed in the public domain and also MIT licensed.
+  See end of file for detailed license information.
+
+*/
+#include "../efb.h"
+
+#include "test.h" /* Simple Testing framework */
+
+int main(void)
+{
+  /*
+  ret
+  */
+  unsigned char ret_undefined[] = {0xC3};
+
+  /*
+  mov eax, 0;
+  ret
+  */
+  unsigned char ret_valid[] = {0xB8, 0x00, 0x00, 0x00, 0x00, 0xC3};
+
+  /* Build executable format */
+  assert(efb_build_executable("ret_undefined.exe", ret_undefined, sizeof(ret_undefined)));
+  assert(efb_build_executable("ret_valid.exe", ret_valid, sizeof(ret_valid)));
+
+  return 0;
+}
+
+/*
+   ------------------------------------------------------------------------------
+   This software is available under 2 licenses -- choose whichever you prefer.
+   ------------------------------------------------------------------------------
+   ALTERNATIVE A - MIT License
+   Copyright (c) 2025 nickscha
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+   of the Software, and to permit persons to whom the Software is furnished to do
+   so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+   ------------------------------------------------------------------------------
+   ALTERNATIVE B - Public Domain (www.unlicense.org)
+   This is free and unencumbered software released into the public domain.
+   Anyone is free to copy, modify, publish, use, compile, sell, or distribute this
+   software, either in source code form or as a compiled binary, for any purpose,
+   commercial or non-commercial, and by any means.
+   In jurisdictions that recognize copyright laws, the author or authors of this
+   software dedicate any and all copyright interest in the software to the public
+   domain. We make this dedication for the benefit of the public at large and to
+   the detriment of our heirs and successors. We intend this dedication to be an
+   overt act of relinquishment in perpetuity of all present and future rights to
+   this software under copyright law.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   ------------------------------------------------------------------------------
+*/
